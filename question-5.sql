c4d52dd0bad8dcb33c254403435a0b15cb56dd57
cliu68 <chunyao.liu0608@gmail.com>
2023-04-11 21:37:25 -0700

    edit Q256

--- question-5.sql
+++ question-5.sql
@@ -1,4 +1,19 @@
 -- What was the most purchased track of 2013?
 select 
-tracks.TrackId
+tracks.TrackId,
+sum(invoice_items.Quantity),
+tracks.Name
+
+from tracks
+
+inner join 
+invoice_items on invoice_items.TrackId = tracks.TrackId
+inner join 
+invoices on invoices.InvoiceId = invoice_items. InvoiceId
+
+where
+invoices.InvoiceDate between '2013-01-01' and '2013-12-31'
+group by tracks.TrackId order by sum(invoice_items.Quantity) DESC;
+
+
 
